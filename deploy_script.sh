0e620ef89b6a5473b18f40ca098b2c8f9d70f9de
Shubham <shubham.kurlekar@panalis.net>
2022-05-23 10:39:21 +0200

    Generalized docker command

--- deploy_script.sh
+++ deploy_script.sh
@@ -7,4 +7,4 @@ echo "Creating docker image..."
 docker build -t clustering_entertainment_news .
 echo "Built Sucessfully"
 echo "Running locally at port 5000"
-docker run --name clustering-entertainment-news -m 0.5g --cpus=0.5 -v /home/shubham/SVK/clustering_entertaiment_news/logs:/clustering_entertainment_news/logs -p 5000:5000 clustering_entertainment_news
+docker run --name clustering-entertainment-news -m 0.5g --cpus=0.5 -v /path/to/repo/clustering_entertaiment_news/logs:/clustering_entertainment_news/logs -p 5000:5000 clustering_entertainment_news
